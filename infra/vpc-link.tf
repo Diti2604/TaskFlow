0b8326f7917d4a65f68e1a550c4a7d60bbd98f3c
Diti <ditiboyboy@gmail.com>
2025-08-25 11:55:25 +0200

    vpc link added

---
+++ infra/vpc-link.tf
@@ -0,0 +1,5 @@
+resource "aws_api_gateway_vpc_link" "vpc-link" {
+  name        = "internal-alb-vpc-link"
+  description = "The VPC Link to connect the internal ALB to the API Gateway"
+  target_arns = [helm_release.aws_lb_controller.arn]
+}
