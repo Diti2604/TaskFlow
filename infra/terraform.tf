78a8aac066d29c992ed023a5c34c863e5b66466b
Diti <ditiboyboy@gmail.com>
2025-08-26 13:56:40 +0200

    s3 bucket name change

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -7,7 +7,7 @@ terraform {
     
   }
   backend "s3" {
-     bucket = "backend-terraform-329458323-9146ty245y2132"
+     bucket = "backend-terraform-329458323-9146ty245y21321"
      key = "backend"
      region = "us-east-1"
 
