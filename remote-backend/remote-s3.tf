c8d445893b23e8a06faf37160295e5feba05c693
Diti <ditiboyboy@gmail.com>
2025-08-26 14:33:14 +0200

    new playground changes

---
+++ remote-backend/remote-s3.tf
@@ -0,0 +1,23 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-${var.account_id}"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+variable "account_id" {
+  type        = string
+  description = "Account ID of new playgrounds"
+  default     = "992382478305"
+}
+
+output "s3-bucket-name" {
+  value = "my-s3-bucket-${var.account_id}"
+}
--- remote-backend/terraform.tf
+++ remote-backend/terraform.tf
@@ -6,10 +6,4 @@ terraform {
     }
     
   }
-  backend "s3" {
-     bucket = "my-s3-bucket-992382478305"
-     key = "backend"
-     region = "us-east-1"
-
-  }
 }
