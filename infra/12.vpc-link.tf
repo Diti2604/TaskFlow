206f598dcacc49cc9605a3a6a7992b49637dc36f
Diti <ditiboyboy@gmail.com>
2025-08-27 14:30:35 +0200

    chnagess+

--- infra/12.vpc-link.tf
+++ infra/12.vpc-link.tf
@@ -1,17 +1,10 @@
 data "aws_lb" "ingress_alb" {
   tags = {
     "app"          = "fastapi"
-    "env"          = "dev"
     "ingress-name" = "fastapi-ingress"
-    "ingress-ns"   = "default"
   }
 }
 
-
-data "aws_lb_listener" "https" {
-  load_balancer_arn = data.aws_lb.ingress_alb.arn
-  port              = 443
-}
 resource "aws_apigatewayv2_api" "http" {
   name          = "http-api"
   protocol_type = "HTTP"
@@ -23,14 +16,12 @@ resource "aws_apigatewayv2_vpc_link" "link" {
   security_group_ids = [aws_vpc.my-vpc.default_security_group_id]
 }
 
-
-
 resource "aws_apigatewayv2_integration" "to_alb" {
   api_id                 = aws_apigatewayv2_api.http.id
   integration_type       = "HTTP_PROXY"
   connection_type        = "VPC_LINK"
   connection_id          = aws_apigatewayv2_vpc_link.link.id
   integration_method     = "ANY"
-  integration_uri        = data.aws_lb_listener.https.arn
+  integration_uri        = data.aws_lb.ingress_alb.arn
   payload_format_version = "1.0"
 }
