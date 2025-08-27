95410d11c4b7a44849f18e87d3faab548aab1490
Diti <ditiboyboy@gmail.com>
2025-08-27 11:21:28 +0200

    changes

--- infra/12.vpc-link.tf
+++ infra/12.vpc-link.tf
@@ -1,6 +1,10 @@
 data "aws_lb" "test" {
   tags = {
-    "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.cert-base.arn
+      "kubernetes.io/ingress.class"            = "alb"
+      "alb.ingress.kubernetes.io/scheme"        = "internal"
+      "alb.ingress.kubernetes.io/listen-ports"  = "[{\"HTTP\":80},{\"HTTPS\":443}]"
+      "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.cert-base.arn
+      "alb.ingress.kubernetes.io/target-type"   = "ip"
   }
 }
 
