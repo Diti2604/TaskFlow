c52b7a96b92d1f81bc0a8893d27b6e999ea4550b
Diti <ditiboyboy@gmail.com>
2025-08-27 14:27:17 +0200

    ch

--- infra/12.vpc-link.tf
+++ infra/12.vpc-link.tf
@@ -6,6 +6,12 @@ data "aws_lb" "ingress_alb" {
     "ingress-ns"   = "default"
   }
 }
+
+
+data "aws_lb_listener" "https" {
+  load_balancer_arn = data.aws_lb.ingress_alb.arn
+  port              = 443
+}
 resource "aws_apigatewayv2_api" "http" {
   name          = "http-api"
   protocol_type = "HTTP"
@@ -25,6 +31,6 @@ resource "aws_apigatewayv2_integration" "to_alb" {
   connection_type        = "VPC_LINK"
   connection_id          = aws_apigatewayv2_vpc_link.link.id
   integration_method     = "ANY"
-  integration_uri        = data.aws_lb.ingress_alb.arn
+  integration_uri        = data.aws_lb_listener.https.arn
   payload_format_version = "1.0"
 }
