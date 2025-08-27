5811e268662ee6e1954abda227dc7f6e952542b4
Diti <ditiboyboy@gmail.com>
2025-08-27 14:36:46 +0200

    changess

--- infra/12.vpc-link.tf
+++ infra/12.vpc-link.tf
@@ -1,9 +1,9 @@
-# data "aws_lb" "ingress_alb" {
-#   tags = {
-#     "app"          = "fastapi"
-#     "ingress-name" = "fastapi-ingress"
-#   }
-# }
+data "aws_lb" "ingress_alb" {
+  tags = {
+    "app"          = "fastapi"
+    "ingress-name" = "fastapi-ingress"
+  }
+}
 
 resource "aws_apigatewayv2_api" "http" {
   name          = "http-api"
@@ -16,12 +16,12 @@ resource "aws_apigatewayv2_vpc_link" "link" {
   security_group_ids = [aws_vpc.my-vpc.default_security_group_id]
 }
 
-# resource "aws_apigatewayv2_integration" "to_alb" {
-#   api_id                 = aws_apigatewayv2_api.http.id
-#   integration_type       = "HTTP_PROXY"
-#   connection_type        = "VPC_LINK"
-#   connection_id          = aws_apigatewayv2_vpc_link.link.id
-#   integration_method     = "ANY"
-#   integration_uri        = data.aws_lb.ingress_alb.arn
-#   payload_format_version = "1.0"
-# }
+resource "aws_apigatewayv2_integration" "to_alb" {
+  api_id                 = aws_apigatewayv2_api.http.id
+  integration_type       = "HTTP_PROXY"
+  connection_type        = "VPC_LINK"
+  connection_id          = aws_apigatewayv2_vpc_link.link.id
+  integration_method     = "ANY"
+  integration_uri        = data.aws_lb.ingress_alb.arn
+  payload_format_version = "1.0"
+}
