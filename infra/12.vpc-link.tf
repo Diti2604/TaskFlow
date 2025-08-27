1caa2deb78c1423aaee530e5916be657d67cf6dc
Diti <ditiboyboy@gmail.com>
2025-08-27 11:19:20 +0200

    chnages

--- infra/12.vpc-link.tf
+++ infra/12.vpc-link.tf
@@ -1,5 +1,12 @@
+data "aws_lb" "test" {
+  tags = {
+    "kubernetes.io/ingress.name" = "fastapi-ingress"
+    "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.cert-base.arn
+  }
+}
+
 resource "aws_api_gateway_vpc_link" "example" {
   name        = "example"
   description = "example description"
-  target_arns = [kubernetes_service_account.alb_controller.metadata[0].name]
+  target_arns = [data.aws_lb.test.arn]
 }
