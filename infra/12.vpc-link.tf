b508ef8920e80fa5faa0f02046f04d13ecae49d3
Diti <ditiboyboy@gmail.com>
2025-08-27 14:23:42 +0200

    changes

--- infra/12.vpc-link.tf
+++ infra/12.vpc-link.tf
@@ -1,18 +1,23 @@
-# data "aws_lb" "test" {
-#   tags = {
-#       "name" = "k8s-default-fastapii-023c11cd46"
-#   }
-# }
 
-# resource "aws_api_gateway_vpc_link" "example" {
-#   name        = "example"
-#   description = "example description"
-#   target_arns = [data.aws_lb.test.arn]
-# }
+resource "aws_apigatewayv2_api" "http" {
+  name          = "http-api"
+  protocol_type = "HTTP"
+}
 
-
-resource "aws_apigatewayv2_vpc_link" "example" {
+resource "aws_apigatewayv2_vpc_link" "link" {
   name          = "alb-vpc-link"
   subnet_ids = slice(aws_subnet.private-subnets[*].id, 0, var.private_subnets_count) 
   security_group_ids = [aws_vpc.my-vpc.default_security_group_id]
+}
+
+
+
+resource "aws_apigatewayv2_integration" "to_alb" {
+  api_id                 = aws_apigatewayv2_api.http.id
+  integration_type       = "HTTP_PROXY"
+  connection_type        = "VPC_LINK"
+  connection_id          = aws_apigatewayv2_vpc_link.link.id
+  integration_method     = "ANY"
+  integration_uri        = data.aws_lb_listener.https.arn
+  payload_format_version = "1.0"
 }
