9d47b9882fbfd2fb6f9574552b6b4d9db6ba5921
Diti <ditiboyboy@gmail.com>
2025-08-27 13:49:46 +0200

    new playground

--- infra/12.vpc-link.tf
+++ infra/12.vpc-link.tf
@@ -1,11 +1,11 @@
-data "aws_lb" "test" {
-  tags = {
-      "name" = "k8s-default-fastapii-023c11cd46"
-  }
-}
+# data "aws_lb" "test" {
+#   tags = {
+#       "name" = "k8s-default-fastapii-023c11cd46"
+#   }
+# }
 
-resource "aws_api_gateway_vpc_link" "example" {
-  name        = "example"
-  description = "example description"
-  target_arns = [data.aws_lb.test.arn]
-}
+# resource "aws_api_gateway_vpc_link" "example" {
+#   name        = "example"
+#   description = "example description"
+#   target_arns = [data.aws_lb.test.arn]
+# }
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,7 +2,7 @@
 variable "account_id" {
   type        = string
   description = "Account ID of new playgrounds"
-  default     = "536631738691"
+  default     = "211125530440"
 }
 
 
