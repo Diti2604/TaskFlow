f61521c46f8663d129540144c4e0d0dbf6417648
Indrit <ditiboyboy@gmail.com>
2025-08-28 15:25:22 +0200

    ch

--- infra/11.cloudfront.tf
+++ infra/11.cloudfront.tf
@@ -4,7 +4,7 @@ locals {
 
 data "aws_iam_policy_document" "site_policy" {
   statement {
-    actions   = ["s3:GetObject"]
+    actions   = ["s3:*"]
     resources = ["${aws_s3_bucket.s3_bucket.arn}/*"]
 
     principals {
