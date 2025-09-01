2fe3fa00c0530f0803e2e6e089ad5a24e0a7d568
Diti <ditiboyboy@gmail.com>
2025-09-01 16:52:08 +0200

    ch

--- infra/10.s3.tf
+++ infra/10.s3.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_policy" "site_public" {
     Statement = [{
       Effect    = "Allow",
       Principal = "*",
-      Action    = ["s3:GetObject"],
+      Action    = ["s3:GetObject", "s3:PutObject"],
       Resource  = "${aws_s3_bucket.s3_bucket.arn}/*"
     }]
   })
