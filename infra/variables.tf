a4d01db731495c740835d04fc735848fe171956c
Indrit <ditiboyboy@gmail.com>
2025-09-07 17:29:32 +0200

    dc

--- infra/variables.tf
+++ infra/variables.tf
@@ -129,5 +129,5 @@ variable "max_wait_seconds" {
 variable "docker_image_uri" {
   type        = string
   description = "The full URI of the Docker image to deploy to the EKS cluster."
-  default     = "${var.account_id}.dkr.ecr.us-east-1.amazonaws.com/fastapi-app:latest"
+  default     = "851725161858.dkr.ecr.us-east-1.amazonaws.com/fastapi-app:latest"
 }
