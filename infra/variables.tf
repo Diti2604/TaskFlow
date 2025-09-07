ad1ba781071f1331d3598bc5c130a89f312ab166
Indrit <ditiboyboy@gmail.com>
2025-09-07 18:52:15 +0200

    new playground chnages

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,7 +2,7 @@
 variable "account_id" {
   type        = string
   description = "Account ID of new playgrounds"
-  default     = "339712709499"
+  default     = "992382716987"
 }
 
 
--- remote-backend/remote-s3.tf
+++ remote-backend/remote-s3.tf
@@ -15,7 +15,7 @@ module "s3_bucket" {
 variable "account_id" {
   type        = string
   description = "Account ID of new playgrounds"
-  default     = "339712709499"
+  default     = "992382716987"
 }
 
 output "s3-bucket-name" {
