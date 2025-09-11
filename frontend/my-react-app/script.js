74e971ccb1cbe1f790726ceb0cbf1c8d77057ce3
Indrit <ditiboyboy@gmail.com>
2025-09-11 11:15:55 +0200

    ch

--- frontend/my-react-app/script.js
+++ frontend/my-react-app/script.js
@@ -5,7 +5,7 @@ let isLogin = true;
 
 
 
-const apiUrl = 'https://po09gfkilb.execute-api.us-east-1.amazonaws.com';
+const apiUrl = 'https://c00uzqy039.execute-api.us-east-1.amazonaws.com';
 
 // Debug function to log issues
 const debugLog = (message) => {
