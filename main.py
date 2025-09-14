e2725bc544ae69e3d10232610a2bba4be6a42d69
Indrit <ditiboyboy@gmail.com>
2025-09-14 21:17:25 +0200

    changes again

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def get_connection():
 
 @app.get("/")
 def root():
-    return {"Hello from FastAPI on EC2"}
+    return {"Hello from FastAPI on EC23456!"}
 
 @app.post("/users")
 def create_user(user: User):
