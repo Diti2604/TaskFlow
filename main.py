58f3f1b6fdbdb16ac45b64b9fcf7807a3cc93e71
Indrit <ditiboyboy@gmail.com>
2025-09-14 13:41:32 +0200

    xhhhhhh

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def get_connection():
 
 @app.get("/")
 def root():
-    return {"Hello from FastAPI on EC2234567890"}
+    return {"Hello from FastAPI on EC22345"}
 
 @app.post("/users")
 def create_user(user: User):
