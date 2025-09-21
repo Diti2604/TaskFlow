eebc734702d6fb74fa61f3bfa60449be45b40543
Indrit <ditiboyboy@gmail.com>
2025-09-21 16:58:38 +0200

    cd

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -14,7 +14,7 @@ terraform {
     }
   }
   backend "s3" {
-     bucket = "my-s3-bucket-355119163536"
+     bucket = "my-s3-bucket-917456273733"
      key = "backend"
      region = "us-east-1"
   }
