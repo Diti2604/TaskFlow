1bec5f53b07a30ae270a6bcc73ac741bde072424
Indrit <ditiboyboy@gmail.com>
2025-09-23 13:13:21 +0200

    change

--- infra/ingress.tf
+++ infra/ingress.tf
@@ -7,9 +7,7 @@ resource "kubernetes_ingress_v1" "fastapi" {
       "alb.ingress.kubernetes.io/scheme"         = "internal"
       "alb.ingress.kubernetes.io/listen-ports"   = "[{\"HTTP\":80},{\"HTTPS\":443}]"
       "alb.ingress.kubernetes.io/certificate-arn"= aws_acm_certificate.cert-base.arn
-      "alb.ingress.kubernetes.io/target-type"    = "ip"
-      "alb.ingress.kubernetes.io/security-groups"  = aws_security_group.alb_from_vpclink_sg.id
-      "alb.ingress.kubernetes.io/inbound-cidrs"    = var.vpc_cidr  
+      "alb.ingress.kubernetes.io/target-type"    = "ip" 
       "alb.ingress.kubernetes.io/tags"           = "app=fastapi,ingress-name=fastapi-ingress"
     }
     }            
