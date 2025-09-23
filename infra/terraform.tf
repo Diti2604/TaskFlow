75a6e0edd9d22b0da2843d268e87edc3464d483f
Indrit <ditiboyboy@gmail.com>
2025-09-23 12:08:55 +0200

    new playground

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -14,7 +14,7 @@ terraform {
     }
   }
   backend "s3" {
-     bucket = "my-s3-bucket-941756836489"
+     bucket = "my-s3-bucket-339712818409"
      key = "backend"
      region = "us-east-1"
   }
