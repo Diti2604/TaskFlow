4685ec42c8004379185b17a2f85e558186cf9333
Indrit <ditiboyboy@gmail.com>
2025-09-23 15:06:33 +0200

    change

--- infra/4.eks.tf
+++ infra/4.eks.tf
@@ -372,3 +372,31 @@ resource "aws_iam_policy" "eks-alb-policy" {
     ]
   })
 }
+
+
+resource "aws_security_group" "ec2_eks_access" {
+  name        = "ec2-eks-access-sg"
+  description = "Allow EC2 instances to access EKS control plane"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-eks-access-sg"
+  }
+}
+
+resource "aws_security_group_rule" "eks_controlplane_ingress" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.ec2_eks_access.id
+  security_group_id        = data.aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+  description              = "Allow EC2 SG to talk to EKS API server"
+}
