939893fd89e8a20809af15994901d2a92b408ed2
Indrit <ditiboyboy@gmail.com>
2025-09-23 23:41:09 +0200

    changes

--- infra/ingress.tf
+++ infra/ingress.tf
@@ -8,6 +8,7 @@ resource "kubernetes_ingress_v1" "fastapi" {
       "alb.ingress.kubernetes.io/listen-ports"   = "[{\"HTTP\":80},{\"HTTPS\":443}]"
       "alb.ingress.kubernetes.io/certificate-arn"= aws_acm_certificate.cert-base.arn
       "alb.ingress.kubernetes.io/target-type"    = "ip" 
+      "alb.ingress.kubernetes.io/healthcheck-path"= "/"
       "alb.ingress.kubernetes.io/tags"           = "app=fastapi,ingress-name=fastapi-ingress"
     }
     }            
