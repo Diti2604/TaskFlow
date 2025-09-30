46dedace647bad27b878785dc091b7bee73eac2e
Diti <ditiboyboy@gmail.com>
2025-09-30 13:36:38 +0200

    ch

--- infra/7.certificate-manager.tf
+++ infra/7.certificate-manager.tf
@@ -1,17 +1,17 @@
-# resource "aws_acm_certificate" "cert-base" {
-#   domain_name               = "${var.account_id}.realhandsonlabs.net"
-#   subject_alternative_names = ["login.${var.account_id}.realhandsonlabs.net", "db.${var.account_id}.realhandsonlabs.net"]
-#   validation_method         = "DNS"
+resource "aws_acm_certificate" "cert-base" {
+  domain_name               = "${var.account_id}.realhandsonlabs.net"
+  subject_alternative_names = ["login.${var.account_id}.realhandsonlabs.net", "db.${var.account_id}.realhandsonlabs.net"]
+  validation_method         = "DNS"
 
-#     tags = {
-#     Project = "fastapi"
-#     Env     = var.account_id
-#     Role    = "ingress-cert"
-#   }
-# }
+    tags = {
+    Project = "fastapi"
+    Env     = var.account_id
+    Role    = "ingress-cert"
+  }
+}
 
-# resource "aws_acm_certificate_validation" "name" {
-#   certificate_arn = aws_acm_certificate.cert-base.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert-validation : record.fqdn ]
-# }
+resource "aws_acm_certificate_validation" "name" {
+  certificate_arn = aws_acm_certificate.cert-base.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert-validation : record.fqdn ]
+}
 
--- infra/8.route53.tf
+++ infra/8.route53.tf
@@ -3,23 +3,23 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
-# resource "aws_route53_record" "cert-validation" {
+resource "aws_route53_record" "cert-validation" {
 
-#   for_each = {
-#     for dvo in aws_acm_certificate.cert-base.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+  for_each = {
+    for dvo in aws_acm_certificate.cert-base.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id = data.aws_route53_zone.main.zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id = data.aws_route53_zone.main.zone_id
+}
 resource "aws_route53_record" "database" {
   zone_id = data.aws_route53_zone.main.zone_id
   name = "db.${var.account_id}.realhandsonlabs.net"
