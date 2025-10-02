a1728d37d031f92eb4c1ee2299070c3efac967bf
Indrit <ditiboyboy@gmail.com>
2025-10-02 16:39:22 +0200

    CF change

--- infra/11.cloudfront.tf
+++ infra/11.cloudfront.tf
@@ -34,9 +34,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
   }
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.imported.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
 }
 data "aws_cloudfront_cache_policy" "caching_disabled" {
