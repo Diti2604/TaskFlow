e99415ee6ebbf3f1b282dd40730cb0d3489c8569
Indrit <ditiboyboy@gmail.com>
2025-10-07 01:46:43 +0200

    main fail

--- infra/7.certificate-manager.tf
+++ infra/7.certificate-manager.tf
@@ -8,16 +8,7 @@ locals {
   ]
 }
 
-# 1) ACME account key
-# ACME account key
 resource "tls_private_key" "acme_account" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-
-# 2) Certificate key (this must match the cert you import to ACM)
-resource "tls_private_key" "cert_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
@@ -27,7 +18,7 @@ resource "acme_registration" "this" {
   email_address           = "email@gmail.com"
 }
 
-resource "acme_certificate" "le" {
+resource "acme_certificate" "acme_cert" {
   account_key_pem           = acme_registration.this.account_key_pem
   common_name               = local.root_domain
   subject_alternative_names = local.names
@@ -36,13 +27,12 @@ resource "acme_certificate" "le" {
 }
 
 resource "aws_acm_certificate" "imported" {
-  private_key       = acme_certificate.le.private_key_pem
-  certificate_body  = acme_certificate.le.certificate_pem
-  certificate_chain = acme_certificate.le.issuer_pem
+  private_key       = acme_certificate.acme_cert.private_key_pem
+  certificate_body  = acme_certificate.acme_cert.certificate_pem
+  certificate_chain = acme_certificate.acme_cert.issuer_pem
 
   tags = { Project = "fastapi", Env = "playground", Role = "cloudfront-cert" }
 }
-
 
 output "acm_arn" {
   value = aws_acm_certificate.imported.arn
