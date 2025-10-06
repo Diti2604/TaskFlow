4d3fa80f23d21ff1ff9832acc0a115c26965ec6f
Indrit <ditiboyboy@gmail.com>
2025-10-06 23:39:22 +0200

    ch

--- infra/7.certificate-manager.tf
+++ infra/7.certificate-manager.tf
@@ -9,10 +9,12 @@ locals {
 }
 
 # 1) ACME account key
+# ACME account key
 resource "tls_private_key" "acme_account" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
+
 
 # 2) Certificate key (this must match the cert you import to ACM)
 resource "tls_private_key" "cert_key" {
@@ -24,28 +26,23 @@ resource "acme_registration" "this" {
   account_key_pem         = tls_private_key.acme_account.private_key_pem
   email_address           = "email@gmail.com"
 }
+
 resource "acme_certificate" "le" {
   account_key_pem           = acme_registration.this.account_key_pem
-  private_key_pem           = tls_private_key.cert_key.private_key_pem   # <<--- IMPORTANT
   common_name               = local.root_domain
   subject_alternative_names = local.names
 
-  dns_challenge {
-    provider = "route53"
-  }
+  dns_challenge { provider = "route53" } 
 }
 
 resource "aws_acm_certificate" "imported" {
-  private_key       = tls_private_key.cert_key.private_key_pem            # <<--- matches above
-  certificate_body  = acme_certificate.le.certificate_pem                 # leaf only
-  certificate_chain = acme_certificate.le.issuer_pem                      # intermediates
+  private_key       = acme_certificate.le.private_key_pem
+  certificate_body  = acme_certificate.le.certificate_pem
+  certificate_chain = acme_certificate.le.issuer_pem
 
-  tags = {
-    Project = "fastapi"
-    Env     = "playground"
-    Role    = "cloudfront-cert"
-  }
+  tags = { Project = "fastapi", Env = "playground", Role = "cloudfront-cert" }
 }
+
 
 output "acm_arn" {
   value = aws_acm_certificate.imported.arn
