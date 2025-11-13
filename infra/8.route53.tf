1d8ef01e1efc5d7a51a98be5b0bec9dfde246c76
Indrit <ditiboyboy@gmail.com>
2025-11-13 12:19:01 +0100

    changes

--- infra/8.route53.tf
+++ infra/8.route53.tf
@@ -34,13 +34,14 @@ resource "aws_route53_record" "cloudmeter" {
   }
 }
 
-# Lookup the ALB created by the AWS Load Balancer Controller for the Kubernetes ingress
+resource "time_sleep" "wait_for_alb" {
+  depends_on = [kubernetes_ingress_v1.fastapi, helm_release.aws_lb_controller]
+  create_duration = "300s"
+}
+
 data "aws_lb" "fastapi_alb" {
-  # find by tag set in ingress annotations: "ingress-name=fastapi-ingress"
-  # Some provider versions expect a tags map rather than filter blocks.
-  tags = {
-    ingress-name = "fastapi-ingress"
-  }
+  depends_on = [time_sleep.wait_for_alb]
+  tags = { ingress-name = "fastapi-ingress" }
 }
 
 # Create a Route53 alias record for the API that points to the ALB
