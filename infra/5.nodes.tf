8f349f51baa418372ad94513ac18bb2279265ac5
Indrit <ditiboyboy@gmail.com>
2025-11-13 14:05:12 +0100

    chanes

--- infra/5.nodes.tf
+++ infra/5.nodes.tf
@@ -33,6 +33,12 @@ resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_on
   role       = aws_iam_role.nodes.name
 }
 
+resource "aws_iam_role_policy_attachment" "worker_nodes_alb_attach" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = aws_iam_policy.eks-alb-policy.arn
+}
+
+
 resource "aws_eks_node_group" "node_group_resource" {
   cluster_name    = aws_eks_cluster.cluster1.name
   node_group_name = var.node_group_name
