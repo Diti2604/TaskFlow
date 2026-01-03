4d02f4e2f42ccc53e9a862503c0eba1f79364ce3
Indrit <ditiboyboy@gmail.com>
2026-01-03 10:55:09 -0600

    changes to the rds security group for connectivity

--- infra/2.rds.tf
+++ infra/2.rds.tf
@@ -32,6 +32,25 @@ resource "aws_security_group" "rds_sg" {
   name        = "rds-sg"
   description = "Allow inbound traffic to the RDS instance"
   vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description     = "Allow MySQL from EKS cluster"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_eks_cluster.cluster1.vpc_config[0].cluster_security_group_id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
 }
 
 
